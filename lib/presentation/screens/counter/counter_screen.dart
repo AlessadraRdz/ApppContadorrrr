076ed964f940520c430d7b7f97789d8f0de7f367
AlessandraRdz <a20490230@itmexicali.edu.mx>
2023-10-27 11:46:40 -0700

    cambios pendientes

--- lib/presentation/screens/counter/counter_screen.dart
+++ lib/presentation/screens/counter/counter_screen.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:guillermo_grellas/presentation/componentes/CustomButtom.dart';
 
 class Counter extends StatefulWidget {
   const Counter({super.key});
@@ -9,43 +10,59 @@ class Counter extends StatefulWidget {
 
 class _CounterState extends State<Counter> {
   int variable = 0;
+  String txt = "Clicks";
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: [
-            Text(
-              '$variable',
-              style:
-                  const TextStyle(fontSize: 160, fontWeight: FontWeight.w100),
-            ),
-            const Text('Clicks', style: TextStyle(fontSize: 60))
-          ],
+        appBar: AppBar(
+            title: const Center(
+                child: Text('Counter', style: TextStyle(fontSize: 60)))),
+        body: Center(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              Text(
+                '$variable',
+                style:
+                    const TextStyle(fontSize: 160, fontWeight: FontWeight.w100),
+              ),
+              Text(variable > 1 ? "Clicks" : "Click",
+                  style: const TextStyle(fontSize: 60))
+            ],
+          ),
         ),
-      ),
-      floatingActionButton: Column(
-        mainAxisAlignment: MainAxisAlignment.end,
-        children: [
-        FloatingActionButton(
-          onPressed: () {
-            setState(() {
-              variable += 1; //Icons.exposure_minus_1
-            });
-          },
-          child: const Icon(Icons.plus_one)),
-          const SizedBox(height: 18),
-           FloatingActionButton(
-          onPressed: () {
-            setState(() {
-              if(variable>0){
-              variable -= 1; //Icons.exposure_minus_1
-              };          
-          });
-          },
-          child: const Icon(Icons.exposure_minus_1)),
-      ],)
-    );
+        floatingActionButton: Column(
+          mainAxisAlignment: MainAxisAlignment.end,
+          children: [
+            CustomButton(
+              icon: Icons.plus_one,
+              onPressed: () {
+                setState(() {
+                  variable++; //Icons.plus_one
+                });
+              },
+            ),
+            const SizedBox(height: 18),
+            CustomButton(
+              icon: Icons.exposure_minus_1,
+              onPressed: () {
+                setState(() {
+                  if (variable > 0) {
+                    variable--; //Icons.exposure_minus_1
+                  }
+                });
+              },
+            ),
+            const SizedBox(height: 18),
+            CustomButton(
+              icon: Icons.exposure_zero,
+              onPressed: () {
+                setState(() {
+                  variable = 0; //Icons.exposure_zero
+                });
+              },
+            )
+          ],
+        ));
   }
 }
